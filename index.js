3425ed7e088c6cd0c5e649bd3527321c346735bf
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-03-11 18:29:40 +0100

    Initial commit.

--- index.js
+++ index.js
@@ -9,28 +9,28 @@
 
 const axios = require('axios');
 
-module.exports = function(date, currencyFrom, amount, currencyTo) {
-  let base_currency = currencyFrom.toUpperCase();
-  let conversion_currency = currencyTo.toUpperCase();
-  return axios
-    .get(`https://api.exchangeratesapi.io/${date}?base=${base_currency}`)
-    .then(json => json.data)
-    .then(function(data){
-        var result = {};
-        if(data.rates.hasOwnProperty(conversion_currency)){
-          let conversion_amount = amount * data.rates[conversion_currency];
-          let fomatted_conversion_amount = parseFloat(conversion_amount.toFixed(4));
-          result = {
-            "date" : date,
-            "base_currency" : base_currency,
-            "base_amount" : amount,
-            "conversion_currency" : conversion_currency,
-            "conversion_amount" : fomatted_conversion_amount
-          }
-        } else {
-          console.log('No conversion currency exist');
-        }
-        return result
-      })
-    .catch(error => console.log(error))
+module.exports = function (date, currencyFrom, amount, currencyTo) {
+    let base_currency = currencyFrom.toUpperCase();
+    let conversion_currency = currencyTo.toUpperCase();
+    return axios
+        .get(`https://api.exchangeratesapi.io/${date}?base=${base_currency}`)
+        .then(json => json.data)
+        .then(function (data) {
+            var result = {};
+            if (data.rates.hasOwnProperty(conversion_currency)) {
+                let conversion_amount = amount * data.rates[conversion_currency];
+                let fomatted_conversion_amount = parseFloat(conversion_amount.toFixed(4));
+                result = {
+                    "date": date,
+                    "base_currency": base_currency,
+                    "base_amount": amount,
+                    "conversion_currency": conversion_currency,
+                    "conversion_amount": fomatted_conversion_amount
+                }
+            } else {
+                console.log('No conversion currency exist');
+            }
+            return result
+        })
+        .catch(error => console.log(error))
 };
